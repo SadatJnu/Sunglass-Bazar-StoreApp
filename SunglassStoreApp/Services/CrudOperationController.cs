22f1db49532eb1eaa430389b8e3816c9efd0907c
Sadat Abdulla Al Muniem <sadatjnu9@gmail.com>
2019-03-28 07:26:13 +0600

    Add files via upload

---
+++ SunglassStoreApp/Services/CrudOperationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SunglassStoreApp.Services
+{
+    public class CrudOperationController : Controller
+    {
+        // GET: CrudOperation
+        public ActionResult ProductCrud()
+        {
+            return View();
+        }
+        public ActionResult CustomerCrud()
+        {
+            return View();
+        }
+        public ActionResult EmployeeCrud()
+        {
+            return View();
+        }
+        public ActionResult AdminDashboard()
+        {
+            return View();
+        }
+
+
+    }//class
+}//ns
